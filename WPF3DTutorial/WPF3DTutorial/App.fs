c4dd538a3fd09e4008f77d280020808ec39d2fdd
fbmnds <fbmnds7@gmail.com>
2014-03-30 14:40:07 +0200

    simple button click

---
+++ WPF3DTutorial/WPF3DTutorial/App.fs
@@ -0,0 +1,21 @@
+﻿module MainApp
+
+open System
+open System.Windows
+open System.Windows.Controls
+open FSharpx
+
+open Gui
+
+type MainWindow = XAML<"MainWindow.xaml">
+
+let loadWindow() =
+   let window = MainWindow()
+
+   (fun sender e -> window.mainViewport.Children.Add(simpleButtonClick())) 
+   |> addButtonHandler (window.simpleButton)
+   
+   window.Root
+
+[<STAThread>]
+(new Application()).Run(loadWindow()) |> ignore
---
+++ WPF3DTutorial/WPF3DTutorial/Gui.fs
@@ -0,0 +1,35 @@
+﻿module Gui
+
+    open System.Windows
+    open System.Windows.Controls
+    open System.Windows.Media
+    open System.Windows.Media.Media3D
+
+
+    let simpleButtonClick() = 
+        let triangleMesh = new MeshGeometry3D()
+        let point0 = new Point3D(0., 0., 0.)
+        let point1 = new Point3D(5., 0., 0.)
+        let point2 = new Point3D(0., 0., 5.)
+        triangleMesh.Positions.Add(point0)
+        triangleMesh.Positions.Add(point1)
+        triangleMesh.Positions.Add(point2)
+        triangleMesh.TriangleIndices.Add(0)
+        triangleMesh.TriangleIndices.Add(2)
+        triangleMesh.TriangleIndices.Add(1)
+        let normal = new Vector3D(0., 1., 0.)
+        triangleMesh.Normals.Add(normal)
+        triangleMesh.Normals.Add(normal)
+        triangleMesh.Normals.Add(normal)
+        let material = new DiffuseMaterial( new SolidColorBrush(Colors.DarkKhaki) )
+        let triangleModel = new GeometryModel3D(triangleMesh, material)
+        let model = new ModelVisual3D()
+        model.Content <- triangleModel
+        model
+
+
+    let addButtonHandler (button: Button) (f: obj -> RoutedEventArgs -> unit) =
+        try
+            button.Click.AddHandler(new RoutedEventHandler( f ) )
+        with
+        | ex -> ex.ToString() |> System.Windows.MessageBox.Show |> ignore
